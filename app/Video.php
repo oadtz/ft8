4639c531446e987bdcea1f74f88b20090b3be69d
tpirmphol <thanapat.pirmphol@chep.com>
2016-08-25 10:02:19 +0700

    no message

--- app/Video.php
+++ app/Video.php
@@ -52,11 +52,11 @@ class Video extends BaseModel
         $text = trim($text);
 
         if (strlen($text) < 10)
-            $size = 200;
-        else if (strlen($text) >= 10 && strlen($text) < 30)
             $size = 150;
-        else
+        else if (strlen($text) >= 10 && strlen($text) < 30)
             $size = 100;
+        else
+            $size = 50;
 
         $image = Image::canvas($w, $h);
         $image->text($text, $w/2, $h-100, function($font) use($color, $size) {
