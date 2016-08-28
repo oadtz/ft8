3b009ac3d79e844a0b639fc58776b7de64fb662d
Thanapat Pirmphol <oadtz@Thanapats-MacBook-Air.local>
2016-08-29 00:07:05 +0700

    no message

--- app/Gif.php
+++ app/Gif.php
@@ -120,7 +120,7 @@ class Gif extends BaseModel
 
         $this->cmd = 'ffmpeg -v warning -i '.$this->outputPath.'/'.static::OUTPUT_FILE_NAME.'.mp4 -vf "scale='.ceil($this->output['width']*config('site.gif_thumbnail_scale')).':'.ceil($this->output['height']*config('site.gif_thumbnail_scale')).':flags=lanczos,palettegen"  -y '.$this->inputPath.'/pallette.png;'.
                     'ffmpeg -v warning -i '.$this->outputPath.'/'.static::OUTPUT_FILE_NAME.'.mp4 -i '.$this->inputPath.'/pallette.png  -lavfi "scale='.ceil($this->output['width']*config('site.gif_thumbnail_scale')).':'.ceil($this->output['height']*config('site.gif_thumbnail_scale')).':flags=lanczos,pad='.max($this->output['thumbnailWidth'], $this->output['thumbnailHeight']).':ih:(ow-iw)/2:color=white [a]; [a][1:v] paletteuse" -y '.$this->inputPath.'/output.gif;'.
-                    'gifsicle -O3 --lossy=160 -o '.$this->outputPath.'/thumbnail.gif '.$this->inputPath.'/output.gif';
+                    'gifsicle -O1 --lossy=200 -o '.$this->outputPath.'/thumbnail.gif '.$this->inputPath.'/output.gif';
 
         //$this->cmd = 'gifsicle -O1 --lossy=120 --scale '.config('site.gif_thumbnail_scale').' -o '.$this->outputPath.'/thumbnail.gif '.$this->outputPath.'/' . static::OUTPUT_FILE_NAME . '.gif;';
 
@@ -229,10 +229,10 @@ class Gif extends BaseModel
         }
 
         if ($output['width'] <= $output['height'])
-            $output['thumbnailWidth'] = $output['thumbnailHeight'] = max($output['width'], $output['height']) * config('site.gif_thumbnail_scale');
+            $output['thumbnailWidth'] = $output['thumbnailHeight'] = ceil(max($output['width'], $output['height']) * config('site.gif_thumbnail_scale'));
         else {
-            $output['thumbnailWidth'] = $output['width'] * config('site.gif_thumbnail_scale');
-            $output['thumbnailHeight'] = $output['height'] * config('site.gif_thumbnail_scale');
+            $output['thumbnailWidth'] = ceil($output['width'] * config('site.gif_thumbnail_scale'));
+            $output['thumbnailHeight'] = ceil($output['height'] * config('site.gif_thumbnail_scale'));
         }
 
         $this->generateCaption($input['width'], $input['height']);
