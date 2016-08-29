583e75bfaee58d332f7262a0b02e89af85b3aaca
Thanapat Pirmphol <oadtz@Thanapats-MacBook-Air.local>
2016-08-29 09:04:46 +0700

    no message

--- config/site.php
+++ config/site.php
@@ -2,8 +2,8 @@
 
 return [
 	'max_file_size'			=>	env('MAX_FILE_SIZE', 209715200),
-	'gif_max_width'			=>	env('GIF_MAX_WIDTH', 500),
-	'gif_thumbnail_width'	=>	env('GIF_THUMBNAIL_WIDTH', 300),
+	'gif_max_width'			=>	env('GIF_MAX_WIDTH', 600),
+	'gif_thumbnail_width'	=>	env('GIF_THUMBNAIL_WIDTH', 470),
 	'gif_max_time'			=>	env('GIF_MAX_TIME', 10),
 	'gif_framerate'			=>	env('GIF_FRAMERATE', 10),
 	'gif_default_caption'	=>	env('GIF_DEFAULT_CAPTION', ' '),
