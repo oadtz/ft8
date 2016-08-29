7c2bf70e0656b673e3fcc5e176ddd0bd63300e78
Thanapat Pirmphol <oadtz@Thanapats-MacBook-Air.local>
2016-08-30 04:02:13 +0700

    no message

--- resources/lang/en/gif.php
+++ resources/lang/en/gif.php
@@ -5,8 +5,8 @@ return [
 			'-1'		=>	'Error',
 			'0'			=>	'Initialized',
 			'1'			=>	'Pending',
-			'2'			=>	'Processing',
-			'3'			=>	'Almost There',
+			'2'			=>	'Generating',
+			'3'			=>	'Final Processing',
 			'4'			=>	'Done',
 			'5'			=>	'Pending for Create GIF Files',
 			'6'			=>	'Completed'
