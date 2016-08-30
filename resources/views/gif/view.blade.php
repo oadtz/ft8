b94ea81bdd4d64d698cae5b9b0839c6f1c3a75e8
tpirmphol <thanapat.pirmphol@chep.com>
2016-08-30 10:28:29 +0700

    no message

--- resources/views/gif/view.blade.php
+++ resources/views/gif/view.blade.php
@@ -10,6 +10,20 @@
 <meta property="og:image" content="{{$gif->thumbnailUrl}}">
 @endsection
 
+@section('javascript')
+@parent
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId={{config('site.facebook_app_id')}}";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+@endsection
+
 @section('content')
 
 <div class="row">
@@ -59,6 +73,18 @@
 			        	<ul class="list-inline">
 			        		<li><a facebook-feed-share class="btn btn-link facebook-share" data-url="{{$gif->url}}" data-shares="shares" title="Share on facebook"><img src="{{url('img/facebook.png')}}" alt="Share on facebook"></a></li>
 			        		<li><a href="http://line.me/R/msg/text/?{{$gif->gifUrl}}" target="_blank" class="btn btn-link" title="Share to LINE"><img src="{{url('img/line.png')}}" alt="Share to LINE"></a></li>
+
+			        		<li>
+			        			<div class="fb-share-button" data-href="{{$gif->url}}" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u={{$gif->url}}F&amp;src=sdkpreparse">Share</a></div>
+			        		</li>
+			        		<li>
+			        			<span>
+									<script type="text/javascript" src="//media.line.me/js/line-button.js?v=20140411" ></script>
+									<script type="text/javascript">
+										new media_line_me.LineButton({"pc":false,"lang":"en","type":"a"});
+									</script>
+								</span>
+			        		</li>
 			        	</ul>
 
 			        	<!--a href>
